b05765733452e7e44077da1f66e55dc63aeac156
Brett Loewen <brettzky007@gmail.com>
2024-01-25 20:04:12 -0600

    Save Load System

--- PlayerSaveData.gd
+++ PlayerSaveData.gd
@@ -1,1 +1,6 @@
-extends Node
+class_name PlayerSaveData
+extends RefCounted
+
+var level: int
+var health: int
+var weapon: Weapon
--- SaveManager.gd
+++ SaveManager.gd
@@ -1,3 +1,5 @@
+# Note: the save/load logic in this script is not perfect. It is just for testing.
+
 extends Control
 
 @export var levelText: Label;
@@ -5,19 +7,73 @@ extends Control
 @export var weaponNameText: Label;
 @export var weaponDamageText: Label;
 
+@export var weapon1: Weapon
+@export var weapon2: Weapon
+
+var playerSaveData: PlayerSaveData
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Load()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func PressLevel():
+	playerSaveData.level += 1
+	UpdateUI()
+	
+func PressHealth():
+	playerSaveData.health += 10
+	UpdateUI()
+	
+func PressWeapon():
+	playerSaveData.weapon = weapon1 if (playerSaveData.weapon.name == "Spear") else weapon2
+	UpdateUI()
 
+func UpdateUI():
+	levelText.text = str(playerSaveData.level)
+	healthText.text = str(playerSaveData.health)
+	weaponNameText.text = playerSaveData.weapon.name
+	weaponDamageText.text = str(playerSaveData.weapon.damage)
 
 func Load():
 	printt("Loading")
-
+	
+	# Create default data if no save data exists
+	if not FileAccess.file_exists("user://save.data"):
+		playerSaveData = PlayerSaveData.new()
+		playerSaveData.level = 1
+		playerSaveData.health = 100
+		playerSaveData.weapon = weapon1
+	else:
+		# Get the file and convert it to json
+		var saveFile = FileAccess.open("user://save.data", FileAccess.READ)
+		var jsonString = saveFile.get_line()
+		saveFile.close()
+		var json = JSON.new()
+		json.parse(jsonString)
+		var data = json.get_data()
+		
+		# If needed, create a new player save data object
+		if playerSaveData == null:
+			playerSaveData = PlayerSaveData.new()
+		
+		# Get the data from the json
+		playerSaveData.level = data["level"]
+		playerSaveData.health = data["health"]
+		playerSaveData.weapon = weapon1 if (data["weapon"] == "Sword") else weapon2
+	
+	UpdateUI()
 
 func Save():
 	printt("Saving")
+	
+	# Write the save data to the file
+	var saveFile = FileAccess.open("user://save.data", FileAccess.WRITE)
+	var data = {
+		"level" : playerSaveData.level,
+		"health" : playerSaveData.health,
+		"weapon" : playerSaveData.weapon.name
+	}
+	var json = JSON.stringify(data)
+	saveFile.store_line(json)
+	saveFile.close()
